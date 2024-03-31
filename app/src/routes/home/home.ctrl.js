5989ed97519c23619e5fa11e9c5e0ed065cf6d9d
이태호 <lth00ce@synapsoft.co.kr>
2024-04-01 03:31:10 +0900

    UserStorage 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,3 +1,5 @@
+import { UserStorage } from '../../models/UserStorage.js';
+
 const output = {
     home: (req, res) => {
         res.render("home/index");
@@ -8,29 +10,25 @@ const output = {
     },
 };
 
-const users = {
-    id: ["woorimIT", "나개발", "김팀장"],
-    psword: ["1234", "1234", "123456"],
-};
-
 const process = {
     login: (req, res) => {
         const id = req.body.id;
         const psword = req.body.psword;
 
+        const users = UserStorage.getUsers("id", "psword");
+
+        const response = {};
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if(users.psword[idx] === psword) {
-                return res.json({
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
 
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response);
     },
 };
 
